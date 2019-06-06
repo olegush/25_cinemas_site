c263981d98d378afabf26b87172609b1836cdcca
olegush <oleg107117@gmail.com>
2019-06-06 12:46:59 +0300

    the code improved, bugs fixed

--- cinemas.py
+++ cinemas.py
@@ -16,11 +16,21 @@ def get_content(url, params, cache):
     cache_content = c.get(cache_id)
     if cache_content is not None:
         return cache_content
-    headers = {'user-agent': UserAgent().chrome}
-    resp = requests.get(url, params=params, headers=headers)
-    content = resp.text
-    c.set(cache_id, content)
-    return content
+    try:
+        headers = {'user-agent': UserAgent().chrome}
+        resp = requests.get(url, params=params, headers=headers)
+        resp.raise_for_status()
+        content = resp.text
+        c.set(cache_id, content)
+        return content
+    except requests.exceptions.Timeout as err:
+        print('TimeoutError:', err)
+    except requests.exceptions.HTTPError as err:
+        print('HTTPError', err)
+    except requests.exceptions.ConnectionError as err:
+        print('ConnectionError', err)
+    except requests.exceptions.RequestException as err:
+        print ('Another Request Error', err)
 
 
 def parse_afisha_page(content):
@@ -31,7 +41,10 @@ def parse_afisha_page(content):
         year = int(movie.find('div', class_='new-list__item-status').string[:4])
         href = movie.find('a', class_='new-list__item-link')['href']
         id_afisha = re.search('\d+', href).group(0)
-        descr = movie.find('div', class_='new-list__item-verdict').string
+        descr = ''
+        descr_tag = movie.find('div', class_='new-list__item-verdict')
+        if descr_tag:
+            descr = descr_tag.string
         movies.append({
             'title': title,
             'year': year,
@@ -46,10 +59,12 @@ def parse_kinopoisk_page(content, year_afisha):
         for movie in soup.find_all('div', class_='element'):
             year_tag = movie.find('span', class_='year')
             year = int(year_tag.string[:4])
+            #print(year)
             rating_tag = year_tag.parent.parent.parent.find('div', class_='rating')
             rating = float(rating_tag.string) if rating_tag else None
             if year == year_afisha:
-                id = year_tag.parent.parent.find('a')['data-id']
-                return rating, id
+                id_kinopoisk = year_tag.parent.parent.find('a')['data-id']
+                return {'rating': rating, 'id_kinopoisk': id_kinopoisk}
     except (TypeError, AttributeError):
         pass
+    return {'rating': None, 'id_kinopoisk': '0'}
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 import time
 
 import concurrent.futures
-
 from flask import Flask, render_template, jsonify
 
 from cinemas import get_content, parse_afisha_page, parse_kinopoisk_page
@@ -12,29 +11,27 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CACHE_AFISHA = {'threshold': 1, 'default_timeout': 60*60*1}
 CACHE_KINOPOISK = {'threshold': 100, 'default_timeout': 60*60*24}
-MOVIES_COUNT = 12
+MOVIES_COUNT = 12 # use -1 for displaying all movies
 
 
 def thread_kinopoisk_function(movie):
     url = 'https://www.kinopoisk.ru/index.php'
     payload = {'kp_query': '{} {}'.format(movie['title'], movie['year'])}
     content = get_content(url, payload, CACHE_KINOPOISK)
-    movie['rating'], movie['id_kinopoisk'] = parse_kinopoisk_page(
-                                                content,
-                                                movie['year'])
+    return parse_kinopoisk_page(content, movie['year'])
 
 
 def films_list():
     payload_afisha = {'view': 'list'}
     url_afisha = 'https://www.afisha.ru/spb/schedule_cinema/'
     content_afisha = get_content(url_afisha, payload_afisha, CACHE_AFISHA)
-    movies = parse_afisha_page(content_afisha)
-
+    afisha_data = parse_afisha_page(content_afisha)
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(thread_kinopoisk_function, movies)
-
+        kinopoisk_data = executor.map(thread_kinopoisk_function, afisha_data)
+    for afisha_movie, kinopoisk_movie in zip(afisha_data, list(kinopoisk_data)):
+        afisha_movie.update(kinopoisk_movie)
     movies = sorted(
-        movies,
+        afisha_data,
         key=lambda x: x['rating'] if x['rating'] else 0,
         reverse=True)
     return movies
@@ -51,6 +48,4 @@ def jsonify_films_list():
 
 
 if __name__ == "__main__":
-    app.debug = os.environ['DEBUG']
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
